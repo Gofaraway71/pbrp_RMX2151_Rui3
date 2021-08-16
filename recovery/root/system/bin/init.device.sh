37e093cab5d3a930e666b6a8893e910c6d864e83
Gofaraway71 <88510377+Gofaraway71@users.noreply.github.com>
2021-08-16 20:32:49 +0100

    Update init.device.sh

--- recovery/root/system/bin/init.device.sh
+++ recovery/root/system/bin/init.device.sh
@@ -1,7 +1,20 @@
-#!/system/bin/sh
-prjVersion=$(cat /proc/oppoVersion/prjVersion)
-if [ "$prjVersion" = 20682 ]; then
-  resetprop ro.build.product RMX2151
-  resetprop ro.product.device RMX2151
-  resetprop ro.product.model RMX2151
-fi
+#!/sbin/sh
+# This script is Needed to Set Automatically Device Props.
+
+prjName=$(cat /proc/oppoVersion/prjVersion)
+echo $prjName
+
+case $prjName in
+    "20682")
+        resetprop "ro.product.model" "Realme 7/ Narzo 20 Pro "
+        resetprop "ro.build.product" "RMX2151"
+        resetprop "ro.product.device" "RMX2151"
+        ;;
+    *)
+        resetprop "ro.product.model" "Realme 6"
+        resetprop "ro.build.product" "RMX2001"
+        resetprop "ro.product.device" "RMX2001"
+        ;;
+esac
+
+exit 0
